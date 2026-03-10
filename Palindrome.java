37260515e085cbd23169fb5b1a263b7046d58a2e
Tinkal Yadav <tinkalyadav9876@gmail.com>
2026-03-10 15:55:53 +0530

    palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Palindrone{
-    public static void main{
-        
-    }
-}
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String word = sc.nextLine();
+
+        String reversed ="";
+        for (int i = word.length()-1; i >=0;i--){
+            reversed = reversed + word.charAt(i);
+        }
+        if (word.equals(reversed)){
+           System.out.println("its a Palindrome");
+        }
+            else{
+            System.out.println("is not Palindrome");
+            }
+            sc.close();
+           }
+        }
